c16ebe99e092b0eadb515586a5fe392ad6dfe901
Goutham <gouthamsricharan18@gmail.com>
2024-09-24 23:33:27 +0530

    step-011

---
+++ lib/services/crud/crud_exceptions.dart
@@ -0,0 +1,17 @@
+class DatabaseALreadyOpenException implements Exception {}
+
+class UnabletogetDocumentsException implements Exception {}
+
+class DatabaseNotOpenException implements Exception {}
+
+class CouldNotDeleteUser implements Exception {}
+
+class UserAlreadyExists implements Exception {}
+
+class CouldNotFindUser implements Exception {}
+
+class CouldNotDeleteNote implements Exception {}
+
+class CouldNotFindNote implements Exception {}
+
+class CouldNotUpdateNote implements Exception {}
