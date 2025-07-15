f11987a4eda7f71fc9995377c884c239bd51ac94
Goutham <gouthamsricharan18@gmail.com>
2025-07-15 12:33:52 +0530

    small change

--- lib/views/notes/notes_list_view.dart
+++ lib/views/notes/notes_list_view.dart
@@ -125,7 +125,7 @@ class _NoteCard extends StatelessWidget {
             borderRadius: BorderRadius.circular(12.0),
             boxShadow: [
               BoxShadow(
-                color: Colors.black.withOpacity(0.1),
+                color: Colors.black.withValues(alpha: 0.1),
                 spreadRadius: 1,
                 blurRadius: 4,
                 offset: const Offset(0, 2),
